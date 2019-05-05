5da16dff81d5f5e81ed210b843747077eb8be794
Felix Lopez <45021056+flx86@users.noreply.github.com>
2019-05-05 15:41:01 -0400

    :hammer: Remove API key from firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase'
 
 var config = {
-   apiKey: "AIzaSyBh1ZJL14SGcjmmoufontcdn-UNgqhgXkU",
-   authDomain: "novilie.firebaseapp.com",
-   databaseURL: "https://novilie.firebaseio.com",
-   projectId: "novilie",
-   storageBucket: "novilie.appspot.com",
-   messagingSenderId: "701221106259"
+   apiKey: "",
+   authDomain: "",
+   databaseURL: "",
+   projectId: "",
+   storageBucket: "",
+   messagingSenderId: ""
  };
 
  const firebaseApp = firebase.initializeApp(config)
